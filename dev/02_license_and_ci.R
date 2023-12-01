bc9ce5e80e57e8b3c8a1eefcb56028c6be929f7e
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-12-01 12:03:30 +0200

    build: mv 02_license_and_ci.R contents to gist

--- dev/02_license_and_ci.R
+++ dev/02_license_and_ci.R
@@ -1,15 +1,1 @@
-s1 <- git2r::status()
-stopifnot(
-  vapply(s1, length, integer(1L)) == 0L
-)
-usethis::use_mit_license()
-unlink(".github", recursive = TRUE, force = TRUE)
-usethis::use_github_action(name = "check-release")
-desc::desc_normalize()
-Sys.sleep(5)
-s2 <- git2r::status()
-
-if (!identical(s1, s2)) {
-  system2("git", c("add", "--all"))
-  system2("git", c("commit", "-m", "\"build: run dev/01_license_and_ci.R\""))
-}
+source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/7bc11bc0015e5708a34e351a2b9932542a7bc536/github_r_pkg_license_and_ci.R")
