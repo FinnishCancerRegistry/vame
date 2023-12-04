cedbc24b66a1c727043668a7fe66096a89192d8a
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-12-04 13:31:51 +0200

    build: revamp dev/02

--- dev/02_license_and_ci.R
+++
@@ -1,1 +0,0 @@
-source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/7bc11bc0015e5708a34e351a2b9932542a7bc536/github_r_pkg_license_and_ci.R")
---
+++ dev/02_misc.R
@@ -0,0 +1,1 @@
+source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/github_r_pkg_license_and_ci.R")
