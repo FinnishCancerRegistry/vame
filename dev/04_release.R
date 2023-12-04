0015c2df892903a4cb09fc86f9383596d5d1e686
Joonas Miettinen <joonas.miettinen@cancer.fi>
2023-12-04 14:02:45 +0200

    build: refactor dev/04_release.R

--- dev/04_release.R
+++ dev/04_release.R
@@ -1,11 +1,1 @@
-message("select version element to bump: [major/minor/patch]")
-desc::desc_bump_version(which = readline(": "))
-
-system2("git", c("add", "DESCRIPTION"))
-v <- as.character(desc::desc_get_version())
-system2("git", c("commit", paste0("-m \"build: v", v, "\"")))
-
-system2("git", c("tag", paste0("v", v)))
-
-system2("git", c("push", "--tags"))
-system2("git", c("push"))
+source("https://gist.githubusercontent.com/WetRobot/a741c9d2a22fbfb52784369be8354f15/raw/r_pkg_release.R")
